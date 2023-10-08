b1651a020c56c3d4ba6d6d31e649afe8d0925d20
AndersonGuassu <andersonguassu@hotmail.com>
2023-10-08 19:22:03 -0300

    gerenciar exames do cliente

--- business/urls.py
+++ business/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('manage_clients/', views.manage_clients, name="manage_clients"),
+    path('client/<int:client_id>', views.client, name="client"),
+    path('exam_client/<int:exam_id>', views.exam_client, name="exam_client"),
+    path('proxy_pdf/<int:exam_id>', views.proxy_pdf, name="proxy_pdf"),
+
 ]
--- business/views.py
+++ business/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from django.db.models import Value
 from django.db.models.functions import Concat
 from django.contrib.admin.views.decorators import staff_member_required
+from exams.models import MedicalTestInquiries
+from django.http import FileResponse
 
 
 @staff_member_required
@@ -21,3 +23,24 @@ def manage_clients(request):
 
     return render(request, 'manage_clients.html',
                   {'clients': clients, 'full_name': full_name, 'email': email})
+
+
+@staff_member_required
+def client(request, client_id):
+    client = User.objects.get(id=client_id)
+    exams = MedicalTestInquiries.objects.filter(user=client)
+    return render(request, 'client.html', {'client': client, 'exams': exams})
+
+
+@staff_member_required
+def exam_client(request, exam_id):
+    exam = MedicalTestInquiries.objects.get(id=exam_id)
+    return render(request, 'exam_client.html', {'exam': exam})
+
+
+@staff_member_required
+def proxy_pdf(request, exam_id):
+    exam = MedicalTestInquiries.objects.get(id=exam_id)
+
+    response = exam.result.open()
+    return FileResponse(response)
